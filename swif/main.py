98c805fa31ee752cb4cfa651be2161f9eea5e9b1
fans656 <fans656@yahoo.com>
2015-12-29 23:45:23 +0800

    2015-12-29 23:45:27

--- swif/main.py
+++ swif/main.py
@@ -10,10 +10,11 @@ from thumbnail import ThumbnailRender
 from f6 import each
 
 WIDTH_HEIGHT_RATIO = 1366.0 / 768
-N_GRID = 3
+N_GRID = float(4)
 COLOR_BASE = QColor('#DFE8EE')
 COLOR_SELECTED = QColor('#616E75')
 COLOR_SHORTCUT = QColor('#C6D4DC')
+SHORTCUTS = 'ASDFQWER1234'
 
 class Window(object):
 
@@ -41,6 +42,7 @@ class Widget(QDialog):
 
         self.cur_index = 0
         self.sticky = False
+        self.simulating = False
         self.resize(QSize(1000, 600))
 
     def on_key(self, ev):
@@ -62,23 +64,27 @@ class Widget(QDialog):
         if not self.isVisible():
             return True
         # alt tab up
-        if ev.KeyID == win32con.VK_LMENU and ev.Message == win32con.WM_KEYUP:
+        if ev.KeyID == win32con.VK_LMENU and ev.Message == win32con.WM_KEYUP \
+                and not self.simulating:
             if self.sticky:
                 self.sticky = False # sticky only once
             else:
-                self.deactivate()
-            # if return False here, the ALT key state is messed (but why?)
+                self.deactivate(lmenu_up=True)
+            return False
         # switch by index
-        elif ev.Key.isdigit():
-            self.select(int(ev.Key) - 1)
+        elif ev.Key in SHORTCUTS:
+            self.select(SHORTCUTS.index(ev.Key))
             self.deactivate()
+            return False
         # stick the switcher '`'
         elif ev.Key == 'Oem_3' and ev.Message in (
             win32con.WM_KEYDOWN, win32con.WM_SYSKEYDOWN):
             self.sticky = True
+            return False
         # cancel
         elif ev.KeyID == win32con.VK_ESCAPE:
-            self.deactivate()
+            self.deactivate(switch=False)
+            return False
         return True
 
     def on_alt_tab_down(self):
@@ -113,6 +119,7 @@ class Widget(QDialog):
         self.show()
         self.refresh()
         self.thumbnail_timer.start(200)
+        QApplication.setActiveWindow(self)
 
     def select(self, index):
         self.cur_index = index
@@ -123,19 +130,41 @@ class Widget(QDialog):
         each(self.windows).render()
         self.update()
 
-    def deactivate(self):
+    def deactivate(self, lmenu_up=False, switch=True):
         if self.isVisible():
             self.thumbnail_timer.stop()
             self.hide()
+            if not switch:
+                return
             hwnd = self.windows[self.cur_index].hwnd
-            win32api.keybd_event(
-                win32con.VK_LMENU, 0,
-                win32con.KEYEVENTF_EXTENDEDKEY, 0
-            )
-            win32api.keybd_event(
-                win32con.VK_LMENU, 0,
-                win32con.KEYEVENTF_EXTENDEDKEY | win32con.KEYEVENTF_KEYUP, 0
-            )
+            # simulate system key event so SetForegroundWindow can succeed
+            self.simulating = True
+            # we need to keep the VK_LMENU up down state
+            # so user needn't release alt to reactive the switcher
+            # after a cancel
+            #
+            # during the process of WM_KEYUP event
+            # pyHook.GetKeyState(win32con.VK_LMENU) will report it's still down
+            # so we use lmenu_up variable here
+            if lmenu_up:
+                # down up
+                win32api.keybd_event(
+                    win32con.VK_LMENU, 0,
+                    win32con.KEYEVENTF_EXTENDEDKEY, 0)
+                win32api.keybd_event(
+                    win32con.VK_LMENU, 0,
+                    win32con.KEYEVENTF_EXTENDEDKEY | win32con.KEYEVENTF_KEYUP,
+                    0)
+            else:
+                # up down
+                win32api.keybd_event(
+                    win32con.VK_LMENU, 0,
+                    win32con.KEYEVENTF_EXTENDEDKEY | win32con.KEYEVENTF_KEYUP,
+                    0)
+                win32api.keybd_event(
+                    win32con.VK_LMENU, 0,
+                    win32con.KEYEVENTF_EXTENDEDKEY, 0)
+            self.simulating = False
             if win32gui.IsIconic(hwnd):
                 cmd_show = win32con.SW_RESTORE
             else:
@@ -159,7 +188,7 @@ class Widget(QDialog):
         rc_all = self.rect()
         x_margin = self.width() / 20.0
         y_margin = self.height() / 10.0
-        thumb_width = (rc_all.width() - x_margin) / 3.0
+        thumb_width = (rc_all.width() - x_margin) / N_GRID
         thumb_height = thumb_width / WIDTH_HEIGHT_RATIO
         thumb_net_width = thumb_width - x_margin
         thumb_net_height = thumb_net_width / WIDTH_HEIGHT_RATIO
@@ -194,7 +223,7 @@ class Widget(QDialog):
                 x_margin / 2,
                 y_margin / 2,
             )
-            p.drawText(rc, Qt.AlignLeft | Qt.AlignTop, str(i + 1))
+            p.drawText(rc, Qt.AlignLeft | Qt.AlignTop, SHORTCUTS[i])
         p.restore()
 
         wnd = self.windows[self.cur_index]
